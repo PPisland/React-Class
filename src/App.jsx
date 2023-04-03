4d3126d83fddbe6b79827f20b2e1be96bc8b01c1
ppisland <a1365gkqls@gmail.com>
2023-04-03 17:57:49 +0900

    react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/main";
+import A from "./pages/a";
+import Header from "./components/Header";
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/a" element={<A />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
